66d3712ad5e7c1e1e9d36db2053ed414c8017cef
Roberto Breve <rbreve@gmail.com>
2013-01-01 12:01:45 -0600

    Initial Commit

---
+++ PlatformB/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  PlatformB
+//
+//  Created by Roberto Breve on 1/1/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
